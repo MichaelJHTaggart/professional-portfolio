3c4e5e848a4e0300f9c464386d974bd07ea9d20b
Michael Taggart <mtaggarthawk@gmail.com>
2021-03-05 12:12:24 -0700

    starting parallaxing

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -7,38 +7,51 @@ const Body = props => {
   win.focus();
  }
 
+ const parallax = document.getElementById("parallax-image-1");
+
+ window.addEventListener("scroll", function () 
+ {
+    let offset = window.pageYOffset;
+    console.log('Offset: ' + offset);
+    console.log('offset * 0.7' + offset * 0.7)
+   //  parallax.style.bagroundPositionY = offset * 0.7 + "px"
+ })
+
  return (
 <div>
 <section className="body">
-  <div className="project-banner-right">
+
+  {/* <div className="project-banner-right">
   <h2>Property Management</h2>
    <p>This website provides a one-stop experience for property managers and tenants. It is built to add and resolve maintenance requests, payments, and to manage properties. The manager view and the tenant view are similar experiences but are two different views.</p>
    
    <div className="project-button" onClick={() => openNewTab('https://handlewhile.com')} aria-label="Check out the website!">Try it out
    </div>
-   </div>
+   </div> */}
    
-   <div>
+   <div id="parallax-image-1">
    <img alt="A preview of the website at the size of a personal computer" class="responsive-image" src="https://lh3.googleusercontent.com/tuZKlKB9xP0ZsoqXV08z75IpW1SDrRK3lVfyMFWki7QkcRdsrMf5UIHQwDO7fJpOFfpyWQ01K2IyzGXsWrklZ4cvUwmus6TIG1PesNwby0vQWNMyKxRPkqZvKmJlzmQB9Q=w1280"/>
    </div>
-   <div className="responsive-image-small-right">
+   <div id="parallax-image-right-fixed">
    <img alt="A preview of the website at the size of a cellphone" class="responsive-image-small" src="https://lh3.googleusercontent.com/uAFDoigB0k_VA82jSvR2NcBZq-5Ai_JZOkreS5p36JE1gLIbJToKzEvpQ8eSEt9mdquk0OoVpzUeqhUkqsudv6u8iPOjzpDzCaVvQ11Go3QWpsXxdeHwHE9AvdW-wfcqKQ=w1280" />
    </div>
+
+
    </section>
 
 <section className="body">
-<div className="project-banner-left">
+{/* <div className="project-banner-left">
    <h2>Time Management</h2>
    <p>Managing time is important in every business. Once you create an account you will be able to track your time for each project that you start! Don't forget to name each project!</p>
    <div className="project-button" onClick={() => openNewTab('https://lemonprop.com')} aria-label="Check out the website!">Try it out
    </div>
-   </div>
+   </div> */}
    
    
-   <div className="resize">
+   <div id="parallax-image-2">
    <img alt="A preview of the website at the size of a personal computer" class="responsive-image" src="https://lh5.googleusercontent.com/-J4_G4q1aZesPQCG_lwkb-B4doFjbwzYIgpgWAzxDiXu3MMWETSres0sfAQtMnwzTAiVZFSHhktkKSQZcOs7hgsdy5CwpnHvj_PofdrRGPIfjVwA9s-YRmdWWnuasFZ1=w1280" />
    </div>
- <div className="responsive-image-small-left">  
+ <div id="parallax-image-left-fixed">  
  <img alt="A preview of the website at the size of a cellphone" src="https://lh6.googleusercontent.com/OCnQZ9TVm0cwWbcvhgNCbKKF7f4kuhL9dQ1leU3qNqK98pcRqAPFkVD4G-ZuiOUvcos5QYwJYGxonWCHJv8NMwN_081cBZexQPkTToidJ3ECOi8q5VeNulEc48iWAap0mQ=w1280" />
 </div>
 </section>
