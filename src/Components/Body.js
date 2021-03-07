9dd4b974eba5cf92705d4d62d61c404ea2b712cc
Michael Taggart <mtaggarthawk@gmail.com>
2021-03-06 22:59:15 -0700

    adding parallax

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,61 +2,61 @@ import '../scss/main.scss'
 
 const Body = props => {
 
- const openNewTab = (url) => {
-  const win = window.open(`${url}`);
-  win.focus();
- }
+  const openNewTab = (url) => {
+    const win = window.open(`${url}`);
+    win.focus();
+  }
 
- const parallax = document.getElementById("parallax-image-1");
-
- window.addEventListener("scroll", function () 
- {
+  window.addEventListener("scroll", function () {
     let offset = window.pageYOffset;
-    console.log('Offset: ' + offset);
-    console.log('offset * 0.7' + offset * 0.7)
-   //  parallax.style.bagroundPositionY = offset * 0.7 + "px"
- })
+    // console.log('Offset: ' + offset);
+    // console.log('offset * 0.7' + offset * 0.7)
+    document.getElementById("parallax-image-1").style.backgroundPositionY = offset * 1 + "px";
+    document.getElementById("parallax-image-right").style.backgroundPositionY = offset * 1 + "px";
+    document.getElementById("parallax-image-2").style.backgroundPositionY = offset * 1 + "px";
+    document.getElementById("parallax-image-left").style.backgroundPositionY = offset * 1 + "px";
+  })
 
- return (
-<div>
-<section className="body">
+  return (
+    <div>
+      <section className="body">
 
-  {/* <div className="project-banner-right">
-  <h2>Property Management</h2>
-   <p>This website provides a one-stop experience for property managers and tenants. It is built to add and resolve maintenance requests, payments, and to manage properties. The manager view and the tenant view are similar experiences but are two different views.</p>
-   
-   <div className="project-button" onClick={() => openNewTab('https://handlewhile.com')} aria-label="Check out the website!">Try it out
+        <div className="project-banner-right">
+          <h2>Property Management</h2>
+          <p>This website provides a one-stop experience for property managers and tenants. It is built to add and resolve maintenance requests, payments, and to manage properties. The manager view and the tenant view are similar experiences but are two different views.</p>
+
+          <div className="project-button" onClick={() => openNewTab('https://handlewhile.com')} aria-label="Check out the website!">Try it out
    </div>
-   </div> */}
-   
-   <div id="parallax-image-1">
-   <img alt="A preview of the website at the size of a personal computer" class="responsive-image" src="https://lh3.googleusercontent.com/tuZKlKB9xP0ZsoqXV08z75IpW1SDrRK3lVfyMFWki7QkcRdsrMf5UIHQwDO7fJpOFfpyWQ01K2IyzGXsWrklZ4cvUwmus6TIG1PesNwby0vQWNMyKxRPkqZvKmJlzmQB9Q=w1280"/>
-   </div>
-   <div id="parallax-image-right-fixed">
-   <img alt="A preview of the website at the size of a cellphone" class="responsive-image-small" src="https://lh3.googleusercontent.com/uAFDoigB0k_VA82jSvR2NcBZq-5Ai_JZOkreS5p36JE1gLIbJToKzEvpQ8eSEt9mdquk0OoVpzUeqhUkqsudv6u8iPOjzpDzCaVvQ11Go3QWpsXxdeHwHE9AvdW-wfcqKQ=w1280" />
-   </div>
+        </div>
+
+        <div id="parallax-image-1">
+          {/* <img alt="A preview of the website at the size of a personal computer" class="responsive-image" src="https://lh3.googleusercontent.com/tuZKlKB9xP0ZsoqXV08z75IpW1SDrRK3lVfyMFWki7QkcRdsrMf5UIHQwDO7fJpOFfpyWQ01K2IyzGXsWrklZ4cvUwmus6TIG1PesNwby0vQWNMyKxRPkqZvKmJlzmQB9Q=w1280" /> */}
+        </div>
+        <div id="parallax-image-right">
+          <img alt="A preview of the website at the size of a cellphone" class="responsive-image-small" src="https://lh3.googleusercontent.com/uAFDoigB0k_VA82jSvR2NcBZq-5Ai_JZOkreS5p36JE1gLIbJToKzEvpQ8eSEt9mdquk0OoVpzUeqhUkqsudv6u8iPOjzpDzCaVvQ11Go3QWpsXxdeHwHE9AvdW-wfcqKQ=w1280" />
+        </div>
 
 
-   </section>
+      </section>
 
-<section className="body">
-{/* <div className="project-banner-left">
-   <h2>Time Management</h2>
-   <p>Managing time is important in every business. Once you create an account you will be able to track your time for each project that you start! Don't forget to name each project!</p>
-   <div className="project-button" onClick={() => openNewTab('https://lemonprop.com')} aria-label="Check out the website!">Try it out
+      <section className="body">
+        <div className="project-banner-left">
+          <h2>Time Management</h2>
+          <p>Managing time is important in every business. Once you create an account you will be able to track your time for each project that you start! Don't forget to name each project!</p>
+          <div className="project-button" onClick={() => openNewTab('https://lemonprop.com')} aria-label="Check out the website!">Try it out
    </div>
-   </div> */}
-   
-   
-   <div id="parallax-image-2">
-   <img alt="A preview of the website at the size of a personal computer" class="responsive-image" src="https://lh5.googleusercontent.com/-J4_G4q1aZesPQCG_lwkb-B4doFjbwzYIgpgWAzxDiXu3MMWETSres0sfAQtMnwzTAiVZFSHhktkKSQZcOs7hgsdy5CwpnHvj_PofdrRGPIfjVwA9s-YRmdWWnuasFZ1=w1280" />
-   </div>
- <div id="parallax-image-left-fixed">  
- <img alt="A preview of the website at the size of a cellphone" src="https://lh6.googleusercontent.com/OCnQZ9TVm0cwWbcvhgNCbKKF7f4kuhL9dQ1leU3qNqK98pcRqAPFkVD4G-ZuiOUvcos5QYwJYGxonWCHJv8NMwN_081cBZexQPkTToidJ3ECOi8q5VeNulEc48iWAap0mQ=w1280" />
-</div>
-</section>
+        </div>
 
-</div>
- );
+
+        <div id="parallax-image-2">
+          {/* <img alt="A preview of the website at the size of a personal computer" class="responsive-image" src="https://lh5.googleusercontent.com/-J4_G4q1aZesPQCG_lwkb-B4doFjbwzYIgpgWAzxDiXu3MMWETSres0sfAQtMnwzTAiVZFSHhktkKSQZcOs7hgsdy5CwpnHvj_PofdrRGPIfjVwA9s-YRmdWWnuasFZ1=w1280" /> */}
+        </div>
+        <div id="parallax-image-left">
+          <img alt="A preview of the website at the size of a cellphone" src="https://lh6.googleusercontent.com/OCnQZ9TVm0cwWbcvhgNCbKKF7f4kuhL9dQ1leU3qNqK98pcRqAPFkVD4G-ZuiOUvcos5QYwJYGxonWCHJv8NMwN_081cBZexQPkTToidJ3ECOi8q5VeNulEc48iWAap0mQ=w1280" />
+        </div>
+      </section>
+
+    </div>
+  );
 }
 export default Body
