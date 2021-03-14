a1be9c88f701806aa995e3d6e9c71e2f9b42df5f
Michael Taggart <mtaggarthawk@gmail.com>
2021-03-13 20:27:34 -0700

    parallax work

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,58 +1,48 @@
 import '../scss/main.scss'
-import lemonLarge from '../assets/images/lemonpropLarge.png'
-import handleLarge from '../assets/images/handlewhileLarge.png'
-import handlePhone from '../assets/images/pHandlewhile.png'
-import lemonPhone from '../assets/images/pLemonprop.png'
+
 
 const Body = props => {
 
   const openNewTab = (url) => {
     const win = window.open(`${url}`);
     win.focus();
-
   }
 
+  window.addEventListener("scroll", function () {
+    const parallax = document.getElementById("parallax");
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+  })
 
   return (
     <div>
-      <section className="body">
+      {/* <div className="project-banner-left">
+        <h2>Property Management</h2>
+        <p>This website provides a one-stop experience for property managers and tenants. It is built to add and resolve maintenance requests, payments, and to manage properties. The manager view and the tenant view are similar experiences but are two different views.</p>
 
-        <div className="project-banner-right">
-          <h2>Property Management</h2>
-          <p>This website provides a one-stop experience for property managers and tenants. It is built to add and resolve maintenance requests, payments, and to manage properties. The manager view and the tenant view are similar experiences but are two different views.</p>
+        <button className="project-button"
+          onClick={() => openNewTab('https://lemonprop.com')} aria-label="Check out the website!">Try it out
+          </button>
+      </div> */}
+      <section id="parallax">
 
-          <button onClick={() => openNewTab('https://handlewhile.com')} aria-label="Check out the website!">Try it out
-   </button>
-        </div>
-
-        <div id="parallax-image-1">
-          <img alt="A preview of the website at the size of a personal computer" class="responsive-image" src={lemonLarge} />
-        </div>
-        <div id="parallax-image-right">
+        {/* <div>
           <img alt="A preview of the website at the size of a cellphone" class="responsive-image-small" src={lemonPhone} />
-        </div>
-
-
+        </div> */}
       </section>
+      {/* <div className="project-banner-right">
+        <h2>Time Management</h2>
+        <p>Managing time is important in every business. Once you create an account you will be able to track your time for each project that you start! Don't forget to name each project!</p>
+        <button className="project-button" onClick={() => openNewTab('https://handlewhile.com')} aria-label="Check out the website!">Try it out
+          </button>
+      </div> */}
+      <section id="parallax">
 
-      <section className="body">
-        <div className="project-banner-left">
-          <h2>Time Management</h2>
-          <p>Managing time is important in every business. Once you create an account you will be able to track your time for each project that you start! Don't forget to name each project!</p>
-          <button className="project-button" onClick={() => openNewTab('https://lemonprop.com')} aria-label="Check out the website!">Try it out
-   </button>
-        </div>
-
-
-        <div id="parallax-image-2">
-          <img alt="A preview of the website at the size of a personal computer" class="responsive-image" src={handleLarge} />
-        </div>
-        <div id="parallax-image-left">
+        {/* <div>
           <img alt="A preview of the website at the size of a cellphone" src={handlePhone} />
-        </div>
+        </div> */}
       </section>
-
-    </div>
+    </div >
   );
 }
 export default Body
